58851cfdf9a81c4822d07c4538402a1fa7e7299a
Parth Jain <parthjain1812@gmail.com>
2025-03-19 19:52:05 +0530

    Add SecurityHeadersMiddleware to enhance security with Content-Security-Policy

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from routes import dashboard, chat, data_entry
+from starlette.middleware.base import BaseHTTPMiddleware
+
+class SecurityHeadersMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response.headers["Content-Security-Policy"] = "upgrade-insecure-requests"
+        return response
 
 app = FastAPI(title="Hotel Booking Analytics")
 
+app.add_middleware(SecurityHeadersMiddleware)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
